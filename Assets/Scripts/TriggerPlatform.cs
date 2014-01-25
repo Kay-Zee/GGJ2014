bf905374ef5e064e616a91107338e35da62a96e4
Kan Zhang <kan.k.zhang@gmail.com>
2014-01-25 13:22:24 -0800

    Base mechanics updated

---
+++ Assets/Scripts/TriggerPlatform.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerPlatform : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+		print (col.gameObject.tag);
+		Transform platform = transform.parent;
+
+		if(col.gameObject.tag == "Player"){
+			print ("in player "+col.gameObject.layer+" "+ platform.gameObject.layer);
+			Physics2D.IgnoreLayerCollision(col.gameObject.layer, platform.gameObject.layer, true);
+
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col){
+		Transform platform = transform.parent;
+		if(col.gameObject.tag == "Player"){
+			print ("exit player");
+			Physics2D.IgnoreLayerCollision(col.gameObject.layer, platform.gameObject.layer, false);
+		}
+	}
+}
