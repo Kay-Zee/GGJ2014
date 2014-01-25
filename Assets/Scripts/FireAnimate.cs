34d189943657c8cb96cc12bce337dcf19db4beea
Your Name <you@example.com>
2014-01-25 12:58:47 -0800

    simple level 02

---
+++ Assets/Scripts/FireAnimate.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireAnimate : MonoBehaviour {
+
+	//NOW THIS SCRIPT IS PLACEHOLDER ONLY
+	//HAVE NOT GOTTEN THIS TO WORK WITH GIF Texture[]
+	//SO THE FIRE IS NOW MANUALLY ANIMATED BY ANIMATING EACH FRAME JUST LIKE THE BIRD
+
+	public Texture[] pictureArray;
+	private Texture picture; 
+	private int delay = 0;
+	private int count = 0;
+	// Use this for initialization
+	void Start () {
+	
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		//OLD CODE HERE 
+
+
+	}
+}
