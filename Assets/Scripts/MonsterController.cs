4cf4d9e0018103fff73c62903d0369d3999ba541
Irene Fung <irene.ifung@gmail.com>
2014-01-25 19:57:05 -0800

    Add monster to level 1
    Add monster controller

---
+++ Assets/Scripts/MonsterController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MonsterController : MonoBehaviour {
+
+	[HideInInspector]
+	public bool facingRight = true;			// For determining which way the monster is currently facing.
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void Flip ()
+	{
+		// Switch the way the player is labelled as facing.
+		facingRight = !facingRight;
+		
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+}
